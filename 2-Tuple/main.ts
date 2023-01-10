96153d52596f5ee66d0b2a14a157e6b1c79a1037
Abhijeet Shende <GS-3467@GSLAB.COM>
2023-01-10 17:21:07 +0530

    Tuple

---
+++ 2-Tuple/main.ts
@@ -0,0 +1,16 @@
+interface Person {
+  name: string;
+  age: number;
+  location: string;
+  hobbies: [string,number];   // creating tuple type
+                              // first elemnt will be string and second elent will be number
+}
+
+const person: Person = {
+  name: 'John Smith',
+  age: 30,
+  location: 'New York',
+  hobbies: ['programming',1] // if pushing third value will show error during creation
+};
+
+person['hobbies'].push('admin') // pushing will work this is an execption
